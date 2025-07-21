ea4e60e7c2dfa773607b3acb2eeb1afc843b4f0d
Martin Gruber <martin.gr.gruber@bmw.de>
2025-07-21 14:44:36 +0200

    autokey: add github search

---
+++ autokey/.config/autokey/data/my_scripts/github_search.py
@@ -0,0 +1,12 @@
+import subprocess
+import urllib
+
+GITHUB_URL = "https://github.com"
+
+ret_code, user_input = dialog.input_dialog(title="GitHub search")
+
+if ret_code == 0:
+    encoded_user_input = urllib.parse.quote(user_input)
+    # search_url = f"https://cc-github.bmwgroup.net/search?q={encoded_user_input}"
+    search_url = f"{GITHUB_URL}/search?q={encoded_user_input}"
+    subprocess.Popen(["google-chrome", search_url])
